0249239312232c2a8e64320675e42190b0b7a54d
martinsrvinicius <martinsrvinicius@gmail.com>
2022-04-25 16:03:41 -0300

    Backend project created

---
+++ backend/src/main/java/com/martinsrvinicius/vrmclient/VrmclientApplication.java
@@ -0,0 +1,13 @@
+package com.martinsrvinicius.vrmclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrmclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrmclientApplication.class, args);
+	}
+
+}
