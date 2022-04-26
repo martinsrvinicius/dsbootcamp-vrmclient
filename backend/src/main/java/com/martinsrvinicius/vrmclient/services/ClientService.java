7e1034ebecbada4f136204ccf96aba79d3061e33
martinsrvinicius <martinsrvinicius@gmail.com>
2022-04-26 16:31:18 -0300

    Database seeding

--- backend/src/main/java/com/martinsrvinicius/vrmclient/services/ClientService.java
+++ backend/src/main/java/com/martinsrvinicius/vrmclient/services/ClientService.java
@@ -4,18 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.martinsrvinicius.vrmclient.entities.Client;
 import com.martinsrvinicius.vrmclient.repositories.ClientRepository;
 
 @Service
 public class ClientService {
-
+	
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Client> findAll(){
 		return repository.findAll();
 	}
+	
 	
 }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_client (name,cpf,income,children, birth_date) VALUES ('Vinicius Martins','323300896867',8690.0,3,'1988-05-12T10:55:00');
+INSERT INTO tb_client (name,cpf,income,children,birth_date) VALUES ('Rosemeri Martins','05981677902', 5890.0, 3, '1986-08-29T10:30:00Z');
