b8d5d7b4f8d4506418a0c30a25cce1777a27e286
martinsrvinicius <martinsrvinicius@gmail.com>
2022-04-29 18:53:32 -0300

    Insert Client

--- backend/src/main/java/com/martinsrvinicius/vrmclient/services/ClientService.java
+++ backend/src/main/java/com/martinsrvinicius/vrmclient/services/ClientService.java
@@ -31,6 +31,18 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return  new ClientDTO(entity);
 	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setIncome(dto.getIncome());
+		entity.setCpf(dto.getCpf());
+		entity.setChildren(dto.getChildren());
+		entity.setBirthDate(dto.getBirthDate());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
 	
 	
 	
