7790231089374aef898399f193ec8b657f7200eb
martinsrvinicius <martinsrvinicius@gmail.com>
2022-04-29 21:31:43 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,2 +1,10 @@
-INSERT INTO tb_client (name,cpf,income,children, birth_date) VALUES ('Vinicius Martins','323300896867',8690.0,3,'1988-05-12T10:55:00');
-INSERT INTO tb_client (name,cpf,income,children,birth_date) VALUES ('Rosemeri Martins','05981677902', 5890.0, 3, '1986-08-29T10:30:00Z');
+INSERT INTO tb_client (created_At, name,cpf,income,children, birth_date) VALUES (NOW(),'Vinicius Martins','32330896867',8690.0,3,'1988-05-12T10:55:00');
+INSERT INTO tb_client (created_At, name,cpf,income,children,birth_date) VALUES (NOW(), 'Rosemeri Martins','05981677902', 5890.0, 3, '1986-08-29T10:30:00Z');
+INSERT INTO tb_client (created_At, name,cpf,income,children, birth_date) VALUES (NOW(), 'José Ricardo', '15984683524', 1490.9,0,'1985-04-12T10:55:00');
+INSERT INTO tb_client (created_At, name,cpf,income,children,birth_date) VALUES (NOW(), 'Rosely Aparecida','02147586325', 4899.69, 2, '2000-08-29T10:30:00Z');
+INSERT INTO tb_client (created_At, name,cpf,income,children, birth_date) VALUES (NOW(), 'Lucas Silva','32345863214',4980.7, 4,'2005-10-01T10:55:00');
+INSERT INTO tb_client (created_At, name,cpf,income,children,birth_date) VALUES (NOW(), 'Arnaldo Coelho','05489950284', 5569.0, 1, '1968-08-29T10:30:00Z');
+INSERT INTO tb_client (created_At, name,cpf,income,children, birth_date) VALUES (NOW(), 'Gilberto Castanha','114587566245', 8125.9,1,'1984-12-12T10:55:00');
+INSERT INTO tb_client (created_At, name,cpf,income,children,birth_date) VALUES (NOW(), 'Carolina Schueck','05988774522', 1989.66, 2, '1990-09-29T10:30:00Z');
+INSERT INTO tb_client (created_At, name,cpf,income,children, birth_date) VALUES (NOW(), 'Hueslon Marques','22145658554',8690.0,0,'1989-03-24T10:55:00');
+INSERT INTO tb_client (created_At, name,cpf,income,children,birth_date) VALUES (NOW(), 'Ana da Silva','21897422589', 2999.60, 1, '1963-06-06T10:30:00Z');
